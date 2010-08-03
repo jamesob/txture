721ef458c074c5a950bc0bce3af399027daa34e9
job <james.obeirne@gmail.com>
2010-08-03 19:50:57 -0400

    Separated all `post` struct business into its own file.

--- src/txture/mvc/model.clj
+++ src/txture/mvc/model.clj
@@ -1,6 +1,8 @@
 (ns txture.mvc.model
   #^{:doc "We're using flatfile storage."}
-  (:require [clojure.contrib.str-utils2 :as str-utils])
+  (:require 
+     [clojure.contrib.str-utils2 :as str-utils]
+     [txture.mvc.models.post :as post])
   (:use 
      clojure.contrib.duck-streams
      txture.config)
@@ -10,12 +12,9 @@
      [java.util Date]
      [java.text DateFormat]))
 
-(defstruct post
-  :body
-  :title
-  :subtitle
-  :date
-  :labels)
+
+;; fns for file retrieval
+;; ----------------------
 
 (defn get-all-posts-seq 
    "Prune the posts directory tree based on file extension, 
@@ -29,12 +28,6 @@
   "Return `fileseq` sorted, decreasing, by last modification time."
   [fileseq]
   (sort #(> (.lastModified %1) (.lastModified %2)) fileseq))
-
-(defn get-post-short-name
-  [file]
-  (let [p (.getPath file)
-        patt (Pattern/compile (str #"([\w-]+)" *posts-ext*))]
-    (last (last (re-seq patt p)))))
 
 (defn get-matching-files-seq
   "Return post files whose ID all match a certain pattern as a seq."
@@ -51,43 +44,10 @@
 ;; fns that convert files to posts
 ;; -------------------------------
 
-(defn postfile->struct
-  "Transform a postfile into a post struct for internal use."
-  [post-file]
-
-  (defn get-metadata
-    "Extract post metadata from lines of file. Expects and returns metadata 
-    as a hashmap,"
-    [lines]
-    (let [metadata-lines (take-while #(not= "" %) lines)
-          metadata-str (reduce str metadata-lines)]
-      (load-string metadata-str)))
-
-  (defn get-body
-    "Extract body from lines of a file."
-    [lines]
-    (drop 1 (drop-while #(not= % "") lines)))
-
-  (defn get-date
-    "Given a file, return its date string."
-    [file]
-    (let [jdate (new Date (.lastModified file))
-          formatter (.. DateFormat getDateInstance)]
-      (.format formatter jdate)))
-
-  (let [lines (read-lines (.getPath post-file))
-        metadata (get-metadata lines)
-        body (get-body lines)
-        date-map {:date (get-date post-file)}
-        name-map {:short-name (get-post-short-name post-file)}
-        curr-post (struct-map post :body body)]
-    (println (count body))
-    (merge curr-post metadata date-map name-map)))
-                                    
-(defn files->structs
+(defn files->posts
   "Convert a seq of files to a seq of post structs."
   [fileseq]
-  (map #(postfile->struct %) fileseq))
+  (map #(post/file->post %) fileseq))
 
 ;; fns used in `mvc.controller`
 ;; -----------------------------
@@ -96,12 +56,12 @@
   "Retrieve posts from disk, fill out a post structs, return them in a seq."
   [& post-ids]
   (let [post-files (map #(get-file-by-id %) post-ids)]
-    (files->structs post-files)))
+    (files->posts post-files)))
 
 (defn get-recent-posts
   "Retrieve the `n` most recent posts."
   [n]
   (let [postfiles (take n (sort-by-time (get-all-posts-seq)))]
-    (files->structs postfiles)))
+    (files->posts postfiles)))
 
 
