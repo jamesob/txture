98b4a812eb0d8ff04925698ff9514976f6f6a170
James O'Beirne <james.obeirne@gmail.com>
2010-08-07 22:41:33 -0400

    Config doc.

--- src/txture/config.clj
+++ src/txture/config.clj
@@ -1,16 +1,24 @@
 (ns txture.config)
 
 (def *author* "p. marlowe")
-(def *title* "snappy title")
+(def *title* "title")
 (def *subtitle* "")
 (def *text-title* "snappy title by philip marlowe")
 (def *description*
   "This is a web log, etc. Hire me.")
+
 (def *keywords* ; site-wide keywords
   "detective, philip marlowe, philip, marlowe")
 
+;; this is a relative path based rooted on the top-level of txture, i.e. if you
+;; leave *posts-dir* defined as "posts/", the posts folder will be alongside
+;; "src/" and "static/".
 (def *posts-dir* "posts/")
+
 (def *posts-ext* ".txt")
+
+;; If you haven't screwed with `src/txture/core.clj`, then any absolute path
+;; referred to within a reference in the HTML originates in `static/`.
 (def *css-loc* "/css/log.css")
 
 (def *num-posts-shown* 6)
